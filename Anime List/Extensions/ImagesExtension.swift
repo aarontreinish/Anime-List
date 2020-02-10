50774e323ac2e044f263d5b9c156eae2ee33c651
Aaron Treinish <atreinish@kwi.com>
2020-02-10 12:01:59 -0500

    Setting limits to image caching

--- Anime List/Extensions/ImagesExtension.swift
+++ Anime List/Extensions/ImagesExtension.swift
@@ -18,6 +18,10 @@ class CustomImageView: UIImageView {
     
     func loadImageUsingCacheWithUrlString(urlString: String) {
         
+        imageCache.countLimit = 100
+        
+        imageCache.totalCostLimit = 50_000_000
+        
         // setup activityIndicator
         activityIndicator.color = .darkGray
         
