131a73a8a3c5f76687bfe25b4e5142322bf90ed6
Aaron Treinish <aarontreinish@fastmail.com>
2020-06-01 12:52:35 -0400

    Uncommenting request review code for production

--- Anime List/Store Review/ReviewService.swift
+++ Anime List/Store Review/ReviewService.swift
@@ -25,7 +25,7 @@ class ReviewService {
     }
     
     private var oneWeekAgo: Date {
-        return Calendar.current.date(byAdding: .day, value: -7, to: Date())!
+        return Calendar.current.date(byAdding: .day, value: -5, to: Date())!
     }
     
     private var shouldRequestReview: Bool {
@@ -38,9 +38,9 @@ class ReviewService {
     }
     
     func requestReview() {
-        //guard shouldRequestReview else { return }
+        guard shouldRequestReview else { return }
         SKStoreReviewController.requestReview()
-        //lastRequest = Date()
+        lastRequest = Date()
     }
     
 }
